edca84a17efb7b91f9cc33aa174e04371054c001
Matthew Wright <mwright71@gmail.com>
2020-05-20 15:22:19 -0500

    final touches

--- app.js
+++ app.js
@@ -6,17 +6,20 @@ let missed = 0;
 
 // Phrases Array
 let phrases = [
-    'when we all get to heaven',
-    'when the role is called up yonder', 
-    'farther along', 
-    'it is well with my soul', 
-    'sweet beaulah land'];
+    'When we All Get to Heaven',
+    'When the Roll is Called Up yonder', 
+    'Farther Along', 
+    'It is Well With my Soul', 
+    'Sweet Beaulah Land'];
 
 // Hide overlay by pressing start game
 
 startButton.addEventListener('click', (e) => {
     if (e.target.textContent === 'Start Game'){
         overlay.style.display = 'none';
+        reset();
+    } else if (e.target.textContent === "Reset Game") {
+        reset();
     }
 });
 
@@ -48,11 +51,77 @@ addPhraseToDisplay(phraseArray);
 
 //Check the Letter
 const checkLetter = (button) => {
-    const letter = document.querySelector('.letter');
-    for (i = 0; i < letter.length; i += 0) {
+    let match = null;
+    const letter = document.querySelectorAll('.letter');
+    for (let i = 0; i < letter.length; i += 1) {
         const chosenLetter = letter[i];
         if (button === chosenLetter.textContent.toLowerCase()) {
-            chosenLetter.className = '.show';
+            chosenLetter.classList.add('show');
+            match = true;
         } 
     }
+    return match;
 };
+
+// Event listener for letter selection
+qwerty.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON'){
+        e.target.classList.add('chosen');
+        e.target.setAttribute('disabled', true);
+    }
+    const letterFound = checkLetter(e.target.textContent);
+    if (letterFound === null && e.target.tagName === 'BUTTON') {
+              let heartLives = document.querySelectorAll('img');
+              heartLives[missed].setAttribute("src", "images/lostHeart.png");
+              missed += 1;
+            }
+            checkWin(checkLetter);
+});
+
+// checkWin Function
+const checkWin = () => {
+    const letter = document.querySelectorAll('.letter');
+    const show = document.querySelectorAll('.show');
+    const title = document.querySelector('.title');
+    if (letter.length === show.length) {
+        setTimeout (() => {
+            overlay.classList.remove('lose');
+            overlay.classList.add('win');
+            title.textContent = 'YOU WIN!';
+            overlay.style.display = 'flex';
+            startButton.textContent = 'Reset Game';
+        }, 1000);
+    }
+    if (missed >= 5) {
+        setTimeout(() => {
+          overlay.classList.add('lose');
+          title.textContent = 'You Lose. Try again?';
+          overlay.style.display = 'flex';
+          startButton.textContent = 'Reset Game';
+        }, 300);
+      }
+}
+
+// Reset Game
+const reset = () => {
+    let buttons = document.querySelectorAll('BUTTON');
+    for ( let i = 0; i < buttons.length; i++) {
+        buttons[i].removeAttribute('disabled');
+        buttons[i].setAttribute('class', '');
+    }
+
+    let heartLives = document.querySelectorAll('img');
+    for ( let i = 0; i < heartLives.length; i++) {
+        heartLives[i].setAttribute("src", "images/liveHeart.png");
+    }
+    overlay.style.display = "none";
+    
+    const ul = document.querySelector('#phrase ul');
+    const currentPhrase = ul.querySelectorAll('li');
+    currentPhrase.forEach((char) => ul.removeChild(char));
+
+    const phraseArray = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(phraseArray);
+
+    missed = 0;
+}
